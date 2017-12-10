90696548fa3400219fc66a6a33f5829a005395a4
bibi <670839606@qq.com>
2017-12-10 22:21:47 +0800

    register

--- shopping/shopping_user/models.py
+++ shopping/shopping_user/models.py
@@ -1,10 +1,11 @@
+#coding=utf-8
 from django.db import models
 
 class UserInfo(models.Model):
-    username = models.CharField(max_length=20)
+    username=models.CharField(max_length=20)
     userpassword = models.CharField(max_length=40)
     useremail = models.CharField(max_length=30)
-    userreseive = models.CharField(max_length=20)
-    useradd = models.CharField(max_length=100)
-    useryoubian = models.CharField(max_length=6)
-    userphone = models.CharField(max_length=11)
+    userreseive = models.CharField(max_length=20,default='')
+    useradd = models.CharField(max_length=100,default='')
+    useryoubian = models.CharField(max_length=6,default='')
+    userphone = models.CharField(max_length=11,default='')
--- shopping/shopping_user/views.py
+++ shopping/shopping_user/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+# coding=utf-8
+from django.shortcuts import render,redirect
+from .models import *
+from hashlib import sha1
 
-# Create your views here.
+def register(request):
+    return render(request,'shopping_user/register.html')
+
+def register_handle(request):
+    post = request.POST
+    username = post.get('user_name')
+    userpassword=post.get('pwd')
+    cuserpassword = post.get('cpwd')
+    useremail = post.get('email')
+    if userpassword != cuserpassword:
+        return redirect('/user/register/')
+    #密码加密
+    s1 = sha1()
+    s1.update(str(userpassword).encode('utf-8'))
+    upwd = s1.hexdigest()
+    #创建对象
+    user = UserInfo()
+    user.username = username
+    print(username)
+    user.userpassword = upwd
+    user.useremail = useremail
+    user.save()
+    return redirect('/user/login/')
+
